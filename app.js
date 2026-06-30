09ac09a5a38a9b5b92c8582067d6bb5f5b60fe7f
Alison <schalison77@gmail.com>
2026-06-30 11:44:56 -0300

    feat: Implementacion de buscador de estilos de decoracion

--- app.js
+++ app.js
@@ -47,25 +47,82 @@ document.getElementById("mostrar")
 
 
 //Buscador:
-const buscador = [
-    "Esmaltado semipermanente",
-    "Paso 1",
-    "Tips y consejos",
-    "Flores pequeñas"
+const generosDeco = [
+    {
+    nombre: "Minimalista",
+    descripcion: "Líneas finas, puntos simples, figuras geométricas básicas y mucho espacio negativo sobre bases transparentes o nude."
+    },
+    {
+    nombre: "Y2k",
+    descripcion: ": Diseños inspirados en los años 2000 con brillos, mariposas, corazones, estrellas pixeladas y colores vibrantes o neón."
+    },
+    {
+    nombre: "Coquette",
+    descripcion: "Estética hiperfemenina caracterizada por el uso de tonos pastel, moños, perlas, encajes y detalles románticos."
+    },
+    {
+    nombre: "Francesa moderna",
+    descripcion: "Variaciones de la manicura clásica que usan colores brillantes, puntas dobles, formas diagonales o acabados metalizados."
+    },
+    {
+    nombre:"Gotico",
+    descripcion: "Gótico: Destaca por su estética oscura, dramática y sofisticada, inspirada en elementos del misticismo, la era victoriana y la cultura punk o alternativa."
+    },
+    {
+    nombre:"Gótico",
+    descripcion: "Destaca por su estética oscura, dramática y sofisticada, inspirada en elementos del misticismo, la era victoriana y la cultura punk o alternativa."
+    }
 ]
 
-function buscarEnPagina() {
-    const texto = document.getElementById("buscador").value.toLowerCase();
+const contInput = document.getElementById("buscador");
+const resultado = document.getElementById("resultado");
+const btnBuscar = document.getElementById("buscar");
 
-    const encontrado = buscador.some(busqueda => busqueda.toLowerCase() === texto)
-    const resultado = document.getElementById("resultado")
 
-    if (encontrado) {
-        resultado.textContent = "Se ha encontrado..."
-    } else {
-        resultado.textContent = "Lo sentimos, no se han encontrado resultados :("
+function buscarGen() {
+    const busqueda = contInput.value.toLowerCase().trim();
+
+    resultado.innerHTML = "";
+
+    if (busqueda === ""){
+        return;
     }
+
+
+    const generosFiltro = generosDeco.filter(genero => 
+        genero.toLowerCase().startsWith(busqueda)
+    );
+     
+    if (generosFiltro.length === 0) {
+        resultado.innerHTML = 'Lo sentimos, no se encontraron resultados para su busqueda :(';
+        return;
+    }
+
+    generosFiltro.forEach((genero)=> {
+        const div = document.createElement("div");
+        div.className = "result"
+        div.textContent = genero;
+        div.style.cursor = "pointer";
+
+        div.addEventListener("click", () => {
+            mostrarDescrip(genero)
+        })
+        resultado.appendChild(div);
+    }
+    );
 }
 
-document.getElementById("buscar").addEventListener("click", buscarEnPagina())
+function mostrarDescripcion(genero) {
+    // Limpia y muestra la descripción
+    resultado.innerHTML = `
+        <div class="descripcion-activa">
+            <h3>${genero.nombre}</h3>
+            <p>${genero.descripcion}</p>
+            <button onclick="buscarGen()">Volver a los resultados</button>
+        </div>
+    `;
+}
 
+
+contInput.addEventListener("input", buscarGen)
+btnBuscar.addEventListener("click", buscarGen)
