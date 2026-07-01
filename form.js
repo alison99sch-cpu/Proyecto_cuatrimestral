3147294a3ffb39966b9c837c885e33648eb2ce08
Alison <schalison77@gmail.com>
2026-06-30 21:08:47 -0300

    feat: Aagrego validación de formulario corregida

---
+++ form.js
@@ -0,0 +1,33 @@
+// Validación del formulario:
+const nombre = document.getElementById("nomb")
+const email = document.getElementById("email")
+const parr = document.getElementById("advertencia")
+const formulario = document.getElementById("form")
+
+formulario.addEventListener("submit", e=> {
+
+    e.preventDefault()
+
+    let warnings = ""
+    let formatoEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    let ingreso = false
+    parr.innerHTML = ""
+
+    if(nombre.value.length <3) {
+        warnings += `⚠️El nombre NO es valido <br>`
+        ingreso = true
+    }
+
+    if(!formatoEmail.test(email.value)) {
+        warnings += `⚠️El email NO es valido <br>`
+        ingreso = true
+    }
+
+    if(ingreso) {
+        parr.innerHTML = warnings
+    }else{
+        parr.innerHTML = `Enviado con exito! ✨`
+    }
+})
+
+
