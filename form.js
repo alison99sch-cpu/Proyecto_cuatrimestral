9be8efca954f816b072aef01236029211e6aae35
Alison <schalison77@gmail.com>
2026-07-01 08:49:43 -0300

    feat: Agrego validación de radio buttons y textarea al formulario

--- form.js
+++ form.js
@@ -2,7 +2,10 @@
 const nombre = document.getElementById("nomb")
 const email = document.getElementById("email")
 const parr = document.getElementById("advertencia")
+const coment = document.getElementById("comentario")
 const formulario = document.getElementById("form")
+
+const radio = document.getElementsByName("consulta")
 
 formulario.addEventListener("submit", e=> {
 
@@ -23,11 +26,21 @@ formulario.addEventListener("submit", e=> {
         ingreso = true
     }
 
+     if (!document.querySelector('input[name="consulta"]:checked')) {
+        warnings += `⚠️Debe seleccionar en qué lo puedo ayudar <br> `
+        ingreso = true
+     }
+
+     if(comentario.value.trim() === "") {
+        warnings += `⚠️Debe escribir su consulta o sugerencia`
+        ingreso = true
+     }
+
     if(ingreso) {
         parr.innerHTML = warnings
     }else{
         parr.innerHTML = `Enviado con exito! ✨`
     }
-})
+}
 
-
+)
